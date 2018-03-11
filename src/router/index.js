257b5aa5141b16937c4a0a1fdd53a630b1e14ad6
MSI\47104 <15732685948@163.com>
2018-03-11 10:24:50 +0800

    reinit

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import login from '../components/login/login'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-//	mode: 'history',
-	base: __dirname,
   routes: [
     {
       path: '/',
-      name: 'login',
-      component: login
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
